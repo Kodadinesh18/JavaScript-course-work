9ec89c0ae1b7ebb9cce28756f102b89241027e4a
Kodadinesh18 <kodadinesh18@gmail.com>
2025-08-05 17:44:04 +0530

    Dinesh Koda

---
+++ day1.js
@@ -0,0 +1,13 @@
+let name="Dinesh"
+console.log(name)
+console.log(name.length)
+let firstname="Koda"
+let lastname="Dinesh"
+console.log(firstname[0])
+console.log(lastname[lastname.length-1])
+console.log("apnacollege"+12)
+str1=""
+str2=" "
+
+console.log(str1.length)
+console.log(str2.length)
