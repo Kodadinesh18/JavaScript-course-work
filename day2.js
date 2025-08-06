b1a7985e203fc975f587adb726b14ce35c541035
Kodadinesh18 <kodadinesh18@gmail.com>
2025-08-06 17:54:26 +0530

    Dinesh Koda

---
+++ day2.js
@@ -0,0 +1,53 @@
+//printing the output:
+
+console.log("my name is dinesh koda!")
+
+
+//linking js to html file:
+//<script src=""></script>
+
+//template literals: or same as formatted string in python
+//we use ` ` 
+//denote value between ${ }
+age=23
+console.log(`your age is ${age}`)
+
+
+//conditional statements:
+// if (condition){
+//   }
+// else if(condition){
+//   }
+//  else{
+//   }
+
+
+//logical operators : AND -&& and OR- ||
+
+//truthy and falsy :
+//falsy values- 0,-0,false,null,undefined,NaN.empty string-""
+//truthy values=Everything else
+
+
+//alert and prompt:
+
+//alert-it shows an alert message in html page
+//alert("hello this is an alert!")
+//prompt- it takes input from the dialogue box:
+//let age= prompt("enter your age")
+
+
+//practice questions:
+
+let num=45
+if (num%10==0){
+    console.log("Good")
+}else{
+    console.log("Bad")
+}
+
+let age=25
+let nam="Dinesh"
+print(`${nam}is ${age} years old`)
+
+
